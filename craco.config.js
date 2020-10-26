05bfcb9220387d13bf1211f914f49d8f95e98333
Roy Li <me@dada.li>
2020-10-26 22:36:39 +0800

    chore: upgrade CRA v4

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,11 @@
 'use strict'
 
-const CracoWorkboxPlugin = require('craco-workbox')
 const pkg = require('./package.json')
 
 process.env.REACT_APP_VERSION = pkg.version
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoWorkboxPlugin,
-    },
-  ],
+  plugins: [],
   style: {
     postcss: {
       plugins: [
--- workbox.config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = options => {
-  options.importWorkboxFrom = 'local';
-  return options;
-};
