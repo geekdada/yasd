74cf30996bb7e436a25bba7a8999f47ba37973a2
Roy Li <me@dada.li>
2020-10-29 23:25:09 +0800

    feat: support hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import {
+  BrowserRouter,
+  BrowserRouterProps,
+  HashRouter,
+  HashRouterProps,
+} from 'react-router-dom'
 
 import './index.css'
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
+const ReactRouter: React.FC<BrowserRouterProps | HashRouterProps> = (args) => {
+  return process.env.REACT_APP_HASH_ROUTER ? (
+    <HashRouter {...(args as HashRouterProps)}>{args.children}</HashRouter>
+  ) : (
+    <BrowserRouter {...(args as BrowserRouterProps)}>
+      {args.children}
+    </BrowserRouter>
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <ReactRouter>
       <App />
-    </BrowserRouter>
+    </ReactRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 )
